dbcdbfccc18ceaefda62a47ac6040a4535943945
chaege_Thinkpad <c.haege@ipc.uni-stuttgart.de>
2019-03-14 16:15:17 +0100

    Made the visual studio project

---
+++ p8-euler/p8-euler.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+
+int main(int argc, char **argv)
+{
+	unsigned int p8_answ = 0;
+
+
+
+
+	std::cout << "The answer to the 8. problem of ProjectEuler.Net is " << p8_answ << ".\n";
+
+	return 0;
+}
